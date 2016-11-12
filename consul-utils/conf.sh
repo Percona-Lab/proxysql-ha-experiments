f7e23e5d70c7d799dee0054da4c2f2a7ccc96d88
Federico Razzoli <federico.razzoli@percona.com>
2016-11-12 01:54:40 +0100

    [utils] remove / at the end of paths

--- consul-utils/conf.sh
+++ consul-utils/conf.sh
@@ -3,13 +3,13 @@ CONSUL_USER='consul'
 # file to download from consul site
 CONSUL_ARCHIVE='consul_0.7.0_linux_amd64.zip'
 # where consul executable should be placed
-CONSUL_PATH='/usr/local/bin/'
+CONSUL_PATH='/usr/local/bin'
 # contains main consul config file
 CONSUL_CONF_FILE='/etc/consul.cnf'
 # contains configuration for services
-CONSUL_CONF_DIR='/etc/consul.d/'
+CONSUL_CONF_DIR='/etc/consul.d'
 # consul datadir
-CONSUL_DATA_DIR='/var/consul/'
+CONSUL_DATA_DIR='/var/consul'
 # stdout and stderr will be redirected here
 CONSUL_LOG='/var/log/consul.log'
 
